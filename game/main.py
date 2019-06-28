5947a6a26c4e50cf2aff90aea7e43502663833a0
RHUL-James-King <james.king.2015@live.rhul.ac.uk>
2019-06-28 22:46:13 +0100

    Navigating between pages worked out via key presses

--- game/main.py
+++ game/main.py
@@ -1,43 +1,69 @@
 import pyglet
 import random
 
+key = pyglet.window.key
 
-def twinkle(window, stars):
-    """
-    Set stars to twinkle in the background
-    """
-    star_to_twinkle = random.randint(0, 3)*2
-    stars.vertices[star_to_twinkle] = random.randint(0, window.width)
-    stars.vertices[star_to_twinkle+1] = random.randint(0, window.height)
+class MenuScreen:
+
+    def __init__(self, window):
+
+        self.label = pyglet.text.Label("Welcome to Asteroids", font_name="Arial", font_size=36,
+                                  x=(window.width // 2) - 10, y=(window.height // 2) - 10,
+                                  anchor_x="center", anchor_y="center")
+        self.fps_display = pyglet.clock.ClockDisplay()
+        self.stars = pyglet.graphics.vertex_list(4, ('v2i', (
+            random.randint(0, window.width), random.randint(0, window.height),
+            random.randint(0, window.width), random.randint(0, window.height),
+            random.randint(0, window.width), random.randint(0, window.height),
+            random.randint(0, window.width), random.randint(0, window.height)
+        )))
+
+        @window.event
+        def on_draw():
+            window.clear()
+            self.stars.draw(pyglet.graphics.GL_POINTS)
+            self.twinkle(window)
+            self.label.draw()
+            self.fps_display.draw()
+
+    def twinkle(self, window):
+        """
+        Set stars to twinkle in the background
+        """
+        star_to_twinkle = random.randint(0, 3)*2
+        self.stars.vertices[star_to_twinkle] = random.randint(0, window.width)
+        self.stars.vertices[star_to_twinkle+1] = random.randint(0, window.height)
 
 
+class Player1Screen:
 
-def launch():
-    """
-    Set up and launch the game
-    """
-    window = pyglet.window.Window()
-    label = pyglet.text.Label("Welcome to Asteroids", font_name="Arial", font_size=36,
-                              x=window.width//2, y=window.height//2,
-                              anchor_x="center", anchor_y="center")
-    fps_display = pyglet.clock.ClockDisplay()
-    stars = pyglet.graphics.vertex_list(4, ('v2i', (
-        random.randint(0, window.width), random.randint(0, window.height),
-        random.randint(0, window.width), random.randint(0, window.height),
-        random.randint(0, window.width), random.randint(0, window.height),
-        random.randint(0, window.width), random.randint(0, window.height)
-    )))
+    def __init__(self, window):
 
-    @window.event
-    def on_draw():
-        window.clear()
-        stars.draw(pyglet.graphics.GL_POINTS)
-        twinkle(window, stars)
-        label.draw()
-        fps_display.draw()
+        self.label = pyglet.text.Label("Play", font_name="Arial", font_size=36,
+                                       x=(window.width // 2) - 10, y=(window.height // 2) - 10,
+                                       anchor_x="center", anchor_y="center")
 
-    pyglet.app.run()
+        @window.event
+        def on_draw():
+            window.clear()
+            self.label.draw()
+
+
+class Controller:
+
+    def __init__(self):
+        self.window = pyglet.window.Window()
+        self.screen = MenuScreen(self.window)
+
+        @self.window.event
+        def on_key_press(symbol, modifiers):
+            if symbol == key.P:
+                self.screen = Player1Screen(self.window)
+            elif symbol == key.H:
+                self.screen = MenuScreen(self.window)
+
+        pyglet.app.run()
 
 
 if __name__ == "__main__":
-    launch()
+    Controller()
