69541038e7186ab0e995569a4e1437853fae92de
RHUL-James-King <james.king.2015@live.rhul.ac.uk>
2019-07-30 14:12:32 +0100

    Added collision detection

--- game/main.py
+++ game/main.py
@@ -1,7 +1,7 @@
 import pyglet
 import random
 from enum import Enum
-from math import cos, sin, pi
+from math import cos, sin, pi, sqrt
 from apscheduler.schedulers.background import BackgroundScheduler
 
 key = pyglet.window.key
@@ -75,6 +75,8 @@ class Particle:
         self.velocity_y = velocity_y
 
     def draw(self):
+        self.centre_x += self.velocity_x
+        self.centre_y += self.velocity_y
         pyglet.graphics.draw(1, pyglet.gl.GL_POINTS, ('v2i', (int(self.centre_x), int(self.centre_y))))
 
 
@@ -191,9 +193,10 @@ class Player1Screen:
             window.clear()
             self.ship.draw()
             self.fps_display.draw()
-            self.particle_update(window, self.particles)
-            for asteroid in self.asteroids:
-                asteroid.draw()
+            self.particles, self.asteroids, self.ship = \
+                self.entity_update(window, self.particles, self.asteroids, self.ship)
+            if self.ship is None:
+                print("Game over")
 
     def particle_update(self, window, particles):
         for particle in particles:
@@ -225,6 +228,108 @@ class Player1Screen:
                 velocity_y = random.randint(-3, -1)
             velocity_x = random.randint(-3, 3)
         self.asteroids.append(Asteroid(start_x, start_y, velocity_x, velocity_y, 15))
+
+    def out_of_window(self, asteroid, window):
+        return (window.height + asteroid.radius < asteroid.centre_y or asteroid.centre_y < -asteroid.radius) or\
+               (window.width + asteroid.radius < asteroid.centre_x or asteroid.centre_x < -asteroid.radius)
+
+    def entity_update(self, window, particles, asteroids, ship):
+        destroyed_particles = []
+        preserved_particles = []
+        preserved_asteroids = []
+        ship = ship
+        for asteroid in asteroids:
+            destroyed_asteroid = False
+            if self.out_of_window(asteroid, window):
+                destroyed_asteroid = True
+            if self.intersecting_ship(asteroid, ship):
+                return preserved_particles, preserved_asteroids, None
+            for particle in particles:
+                if self.is_inside(particle.centre_x, particle.centre_y, asteroid):
+                    destroyed_asteroid = True
+                    destroyed_particles.append(particle)
+            if not destroyed_asteroid:
+                preserved_asteroids.append(asteroid)
+                asteroid.draw()
+        for particle in particles:
+            if particle not in destroyed_particles and\
+                    0 < particle.centre_x < window.width and 0 < particle.centre_y < window.height:
+                particle.draw()
+                preserved_particles.append(particle)
+        return preserved_particles, preserved_asteroids, ship
+
+    def intersecting_ship(self, asteroid, ship):
+        # Detection adapted from http://www.phatcode.net/articles.php?id=459
+        v1x = int(ship.centre_x + (2 * ship.height * cos(ship.facing)))
+        v1y = int(ship.centre_y + (2 * ship.height * sin(ship.facing)))
+        v2x = int(ship.centre_x + (ship.height * cos(ship.facing + 140)))
+        v2y = int(ship.centre_y + (ship.height * sin(ship.facing + 140)))
+        v3x = int(ship.centre_x + (ship.height * cos(ship.facing - 140)))
+        v3y = int(ship.centre_y + (ship.height * sin(ship.facing - 140)))
+        # Check if the vertices of the ship are intersecting the asteroid
+        if self.is_inside(v1x, v1y, asteroid) or\
+                self.is_inside(v2x, v2y, asteroid) or\
+                self.is_inside(v3x, v3y, asteroid):
+            return True
+        # Check if circle center inside the ship
+        if ((v2y - v1y) * (asteroid.centre_x - v1x) - (v2x - v1x) * (asteroid.centre_y - v1y)) >= 0  and \
+            ((v3y - v2y) * (asteroid.centre_x - v2x) - (v3x - v2x) * (asteroid.centre_y - v2y)) >= 0 and \
+            ((v1y - v3y) * (asteroid.centre_x - v3x) - (v1x - v3x) * (asteroid.centre_x - v3x)) >= 0:
+           
+            return True
+        # Check if edges intersect circle
+        # First edge
+        c1x = asteroid.centre_x - v1x
+        c1y = asteroid.centre_y - v1y
+        e1x = v2x - v1x
+        e1y = v2y - v1y
+
+        k = c1x * e1x + c1y * e1y
+
+        if k > 0:
+            length = sqrt(e1x * e1x + e1y * e1y)
+            k = k / length
+            if k < length:
+                if sqrt(c1x * c1x + c1y * c1y - k * k) <= asteroid.radius:
+                    return True
+
+        # Second edge
+        c2x = asteroid.centre_x - v2x
+        c2y = asteroid.centre_y - v2y
+        e2x = v3x - v2x
+        e2y = v3y - v2y
+
+        k = c2x * e2x + c2y * e2y
+
+        if k > 0:
+            length = sqrt(e2x * e2x + e2y * e2y)
+            k = k / length
+            if k < length:
+                if sqrt(c2x * c2x + c2y * c2y - k * k) <= asteroid.radius:
+                    return True
+
+        # Third edge
+        c3x = asteroid.centre_x - v3x
+        c3y = asteroid.centre_y - v3y
+        e3x = v1x - v3x
+        e3y = v1y - v3y
+
+        k = c3x * e3x + c3y * e3y
+
+        if k > 0:
+            length = sqrt(e3x * e3x + e3y * e3y)
+            k = k / length
+            if k < length:
+                if sqrt(c3x * c3x + c3y * c3y - k * k) <= asteroid.radius:
+                    return True
+        return False
+
+    def is_inside(self, x, y, circle):
+        if ((x - circle.centre_x) * (x - circle.centre_x) + (y - circle.centre_y) * (y - circle.centre_y)
+                <= circle.radius * circle.radius):
+            return True
+        else:
+            return False
 
 
 class State(Enum):
