9c457606586f07b4b5cc581c5feeb31002f7ec74
JCKing97 <jck100notout@hotmail.co.uk>
2020-07-30 22:57:42 +0100

    Basic window detection

--- agents/object_detection_agent.py
+++ agents/object_detection_agent.py
@@ -1,6 +1,6 @@
 from typing import Type
 import os
-import pyglet
+import numpy as np
 from time import time
 from PIL import ImageGrab
 
@@ -8,7 +8,7 @@ from game.agent import Agent, Action
 from game.perception import Perception, ImagePerception, VectorPerception
 
 from agents.decide import attack_nearest_asteroid
-from agents.perceive import get_closest_asteroid_from_image
+from agents.perceive import get_closest_asteroid_from_image, detect_window_in_image
 from agents.reactive_agent import ReactiveAgent
 
 
@@ -42,12 +42,16 @@ class ObjectDetectionTrainingAgent(ReactiveAgent):
     def __init__(self, ship):
         super().__init__(ship)
         self.last_recorded_image_time = time()
+        template_dir = "training_images/templates"
+        self.templates = [os.path.join(template_dir, template) for template in os.listdir(template_dir)]
 
     def perceive(self, perception: VectorPerception):
         super().perceive(perception)
         current_time = time()
         if current_time - self.last_recorded_image_time > 2:
             image = ImageGrab.grab()
+            cv_img = np.asarray(image).copy()
+            detect_window_in_image(self.templates, )
             next_image_num = len(os.listdir("training_images"))
             image.save("training_images/image_{}.png".format(next_image_num))
             self.last_recorded_image_time = current_time
--- agents/perceive.py
+++ agents/perceive.py
@@ -1,6 +1,31 @@
 from typing import Tuple, List
 from pyglet.image import ColorBufferImage
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
 
 
 def get_closest_asteroid_from_image(image: ColorBufferImage) -> Tuple[List[int], int]:
     return [1, 2], 3
+
+
+def detect_window_in_image(templates: List[str], img: str):
+    img = cv2.imread(img, cv2.IMREAD_GRAYSCALE)
+    cv_img2 = img.copy()
+    cv_img = cv_img2.copy()
+    for template in templates:
+        template_image = cv2.imread(template, cv2.IMREAD_GRAYSCALE)
+        w, h = template_image.shape[::-1]
+        res = cv2.matchTemplate(cv_img, template_image, cv2.TM_CCOEFF)
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+        top_left = max_loc
+        bottom_right = (top_left[0] + w, top_left[1] + h)
+        cv2.rectangle(cv_img, top_left, bottom_right, 255, 2)
+
+        plt.subplot(121), plt.imshow(res, cmap='gray')
+        plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
+        plt.subplot(122), plt.imshow(cv_img, cmap='gray')
+        plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
+        plt.suptitle(cv2.TM_CCOEFF)
+        plt.show()
+        print("called show")
